f0d8d66479e4cb77dc576e9fac388b961825a11c
manikanta <>
2023-09-02 12:47:40 +0530

    added project in the folder

---
+++ Social-login-application/src/test/java/com/practice/Socialloginapplication/SocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.Socialloginapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
